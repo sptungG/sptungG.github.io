4e0ac23ba10b8df7c3ce320fcb783900682193d7
sptungG <ngoviettung154@gmail.com>
2021-02-06 11:05:05 +0700

    animation

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ const sr = ScrollReveal({
   duration: 1500,
   reset: true,
 });
-sr.reveal(`.imgBx, .contentBx,
+sr.reveal(`.banner-container,
           .about-data, .about-img,.grid
 `, {
   interval: 100
