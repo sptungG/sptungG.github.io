d71b423eccddfdc341a84af0f026e60a0954b423
sptungG <ngoviettung154@gmail.com>
2021-02-06 10:27:54 +0700

    a

--- main.js
+++ main.js
@@ -83,9 +83,9 @@ tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".imgBx", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-tl.fromTo(".contentBx", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".contentBx", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "-=1");
 tl.fromTo(".controls", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-tl.fromTo(".pagination-button", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
   origin: 'top',
