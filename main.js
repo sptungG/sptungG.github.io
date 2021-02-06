5170f508b8ddbd2c34cebe5de64119dd71ab89b6
sptungG <ngoviettung154@gmail.com>
2021-02-06 10:36:18 +0700

    a

--- main.js
+++ main.js
@@ -82,9 +82,9 @@ tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
 tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo(".imgBx", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-tl.fromTo(".contentBx", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "-=1");
-tl.fromTo(".controls", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".imgBx", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "-=1");
+tl.fromTo(".contentBx", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".controls", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "-=1");
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
@@ -93,11 +93,11 @@ const sr = ScrollReveal({
   duration: 1500,
   reset: true,
 });
-sr.reveal(`
+sr.reveal(`.imgBx, .contentBx,
           .about-data, .about-img,.grid
 `, {
-  interval: 200
-})
+  interval: 100
+});
 /*====================  ====================*/
 
 
