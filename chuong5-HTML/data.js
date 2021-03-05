7e51d5804ac308f793130f3b40e5651abdb81839
sptungG <ngoviettung154@gmail.com>
2021-03-05 19:30:10 +0700

    up chapV

---
+++ chuong5-HTML/data.js
@@ -0,0 +1,22 @@
+let    timeSheetData = [
+    {
+        Project: 'Learn front-end',
+        Task: 'Learn HTML',
+        'Time Spent': 6,
+    },
+    {
+        Project: 'Learn front-end',
+        Task: 'Learn CSS',
+        'Time Spent': 8,
+    },
+    {
+        Project: 'Learn front-end',
+        Task: 'Learn JS Variables and Data Types',
+        'Time Spent': 6,
+    },
+    {
+        Project: 'Learn git',
+        Task: 'Learn git basics',
+        'Time Spent': 2,
+    }
+]; 
