0a2c974d619e83e31ddae8e3aff2f8a52ea2b30e
sptungG <ngoviettung154@gmail.com>
2021-08-14 23:02:20 +0700

    update header

---
+++ furniShop/JS/firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBGD0FRea1qvPAkCy88ldfaTRcKstrVzGk",
+  authDomain: "furnishop-a41c5.firebaseapp.com",
+  projectId: "furnishop-a41c5",
+  storageBucket: "furnishop-a41c5.appspot.com",
+  messagingSenderId: "961452173346",
+  appId: "1:961452173346:web:8a2175e4e149724db3027b",
+  measurementId: "G-B2L9E6JS3R"
+};
