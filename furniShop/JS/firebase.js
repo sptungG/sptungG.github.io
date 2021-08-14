f0ab00db511da4d570164157cbd854ce8755f921
sptungG <ngoviettung154@gmail.com>
2021-08-14 23:05:20 +0700

    update firebase

--- furniShop/JS/firebase.js
+++ furniShop/JS/firebase.js
@@ -5,5 +5,6 @@ var firebaseConfig = {
   storageBucket: "furnishop-a41c5.appspot.com",
   messagingSenderId: "961452173346",
   appId: "1:961452173346:web:8a2175e4e149724db3027b",
-  measurementId: "G-B2L9E6JS3R"
+  measurementId: "G-B2L9E6JS3R",
 };
+var db = firebase.firestore();
